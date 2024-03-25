4b308625710387a911f0a6f6f9f3d99a0985c358
assiajsaad <assia.j.saad@gmail.com>
2024-03-25 22:52:01 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import { Data } from './data';
-import { renderProject } from './components/content';
+import { renderProject } from './dom-manipulation/content';
 
 const data = new Data();
 data.printData();
