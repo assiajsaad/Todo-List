177cb75eced53d6993a4ae54e31fa35ff245ab3e
assiajsaad <assia.j.saad@gmail.com>
2024-03-27 03:14:17 +0200

    Implemented sidebar-list

--- src/components/sidebar-list.js
+++ src/components/sidebar-list.js
@@ -1,8 +1,20 @@
-function showSidebar(){
+function showSidebar(data) {
+  const titleDiv = document.querySelector(".project-list");
+  const titleList = document.createElement("ul");
+  const lastChild = titleDiv.lastElementChild;
 
+  if (lastChild !== null) {
+    titleDiv.removeChild(lastChild);
+  }
+
+  data.map((proj, index) => {
+    const title = document.createElement("li");
+    title.textContent = proj.getTitle;
+    title.classList.add(index);
+    titleList.appendChild(title);
+  });
+
+  titleDiv.appendChild(titleList);
 }
 
-
-export {
-    showSidebar
-}
+export { showSidebar };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style.css';
 import { Data } from './data';
 import { renderProject } from './components/content';
+import { showSidebar } from './components/sidebar-list';
 
 const data = new Data();
 const obj = data.getProjectByIndex(0);
 
-
+showSidebar(data.getData());
+showSidebar(data.getData());
+showSidebar(data.getData());
+showSidebar(data.getData());
 renderProject(obj);
 
